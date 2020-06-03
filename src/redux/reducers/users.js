d8492f1dd80096b845ca1d48578c1b9bd527bd07
Hamed Farag <hamed.farag.2009@gmail.com>
2020-06-03 23:31:42 +0200

    feat: Add Basic Redux Setup
    Create Reducers
    Create Store
    Create Consts Files
    Add Router

---
+++ src/redux/reducers/users.js
@@ -0,0 +1,13 @@
+import consts from "../consts/user";
+
+const initialState = {
+  loggedInUser: {},
+  users: [],
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
