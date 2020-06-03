aeceeada601ffec03cc9b494c732c9bba26b4ee9
Hamed Farag <hamed.farag.2009@gmail.com>
2020-06-04 00:52:27 +0200

    feat: Add Route File
    Add Route File to build the app routes

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import RegisterPage from "./pages/Register";
+
+const publicPages = [
+  {
+    id: "a9sd8fusndf",
+    component: RegisterPage,
+    path: "/register",
+  },
+];
+
+const privatePages = [];
+
+export default {
+  publicPages,
+  privatePages,
+};
