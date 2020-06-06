6f0b16dd45e697478d4f5f8fc8a9e771cc508b04
Hamed Farag <hamed.farag.2009@gmail.com>
2020-06-06 14:25:07 +0200

    feat: Enhance Card Component Styles

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,5 +3,12 @@ import React from "react";
 import "./styles.scss";
 
 export default function Card(props) {
-  return <div className="card-container">{props.children}</div>;
+  return (
+    <div className="card-container">
+      {props.title && (
+        <div className="card-container__header">{props.title}</div>
+      )}
+      <div className="card-container__body">{props.children}</div>
+    </div>
+  );
 }
